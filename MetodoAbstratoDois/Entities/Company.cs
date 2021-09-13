974afc35713111c5b5c4cc02e72b8d41340841b4
Lucas Ortis <lucasortis01@gmail.com>
2021-09-13 19:54:32 -0300

    Class implementation

---
+++ MetodoAbstratoDois/Entities/Company.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MetodoAbstratoDois.Entities
+{
+    class Company : Taxpayer
+    {
+
+        public int NumberOfEmployees { get; set; }
+
+        public Company(string name, double anualIncome, int numberOfEmployees) 
+            : base(name, anualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double TaxesPaid()
+        {
+            if(NumberOfEmployees > 10)
+            {
+                return AnualIncome * 0.14;
+            } 
+            else
+            {
+                return AnualIncome * 0.16;
+            }
+        }
+    }
+}
---
+++ MetodoAbstratoDois/Entities/Taxpayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MetodoAbstratoDois.Entities
+{
+    abstract class Taxpayer
+    {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        public Taxpayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        public abstract double TaxesPaid();
+    }
+}
